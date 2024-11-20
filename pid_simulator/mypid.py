1efbab803854013c72757a70cf85305bbf9dbca0
Jorge Elias <eliasmaxil@hotmail.com>
2024-11-20 17:26:54 +0100

    Backup nov 24

---
+++ pid_simulator/mypid.py
@@ -0,0 +1,5 @@
+# Refs:
+# https://medium.com/@bsmith4360/building-a-simulated-pid-controller-in-python-111b08ccae1a
+# https://github.com/bsmith43600/PID_Simulator/blob/main/simulated_PID_controller.py
+
+
